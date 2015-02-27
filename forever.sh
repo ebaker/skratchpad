0a7857987c4e1d01c9e25be979c39cdd65446145
Eliot Baker <eliotbaker@gmail.com>
2015-02-26 22:33:15 -0500

    forever script updated

--- forever.sh
+++ forever.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export PORT=4001
-forever start serverjs -o /tmp/skratchpad-out.log -e /tmp/skratchpad-error.log
+forever start server.js -o /tmp/skratchpad-out.log -e /tmp/skratchpad-error.log
