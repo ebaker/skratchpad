3062d502fcf6094cdb8ecae0745c1324fb823c3a
Eliot Baker <eliotbaker@gmail.com>
2015-02-28 23:00:42 -0500

    read skratches

--- public/skratchpad.js
+++ public/skratchpad.js
@@ -1,145 +1,36 @@
-// SkratchList
-// -------
-// A place to jot your notes and knowledge
-// Inspired by TodoMVC contributed by
-// [Jérôme Gravel-Niquet](http://jgn.me/).
+//
+var app = angular.module('skratchpadApp',[]);
 
-// Load the application once the DOM is ready, using `jQuery.ready`:
-$(function(){
-
-  // Skratch Model
-  // --------
-  var Skratch = Backbone.Model.extend({
-    defaults: function() {
-      return {
-        text: 'empty text...',
-        date_created: new Date().getTime()
-      };
+app.directive('skratchpad', function(skratchesFactory) {
+  return {
+    scope: {},
+    restrict: 'E',
+    replace: true,
+    link: function(scope, elem, attrs){
+      skratchesFactory.getAll().then(function(data){
+        console.log('data', data);
+        scope.skratches = data;
+      });
     },
-    idAttribute: "_id"
-  });
+    templateUrl: 'skratchpad.html'
+  }
+});
 
-  // SkratchList
-  // -------
-  var SkratchList = Backbone.Collection.extend({
-    model: Skratch,
+app.factory('skratchesFactory', function($http, $q) {
+  return {
+    getAll: function() {
+      var defer = $q.defer()
+      $http.get('/api/skratches').success(function(data){
+        // return data;
+        defer.resolve(data);
 
-    url: '/api/skratches'
-    // localStorage: new Backbone.LocalStorage("skratches-backbone")
-  });
-
-  var Skratches = new SkratchList();
-
-  // Skratch View
-  // --------------
-
-  // The DOM element for a Skratch item...
-  var SkratchView = Backbone.View.extend({
-
-    tagName:  "li",
-
-    template: _.template($('#skratch-template').html()),
-
-    events: {
-      "click a.update"  : "edit",
-      "click a.destroy" : "clear",
-      "keypress .edit"  : "updateOnEnter",
-      "blur .edit"      : "close"
-    },
-
-    initialize: function() {
-      this.listenTo(this.model, 'change', this.render);
-      this.listenTo(this.model, 'destroy', this.remove);
-    },
-
-    render: function() {
-      this.$el.html(this.template(this.model.toJSON()));
-      this.$el.addClass('list-group-item');
-      this.input = this.$('.edit');
-      return this;
-    },
-
-    edit: function() {
-      this.$el.addClass("editing");
-      this.input.focus();
-    },
-
-    close: function() {
-      var value = this.input.val();
-      if (!value) {
-        this.clear();
-      } else {
-        this.model.save({text: value});
-        this.$el.removeClass("editing");
-      }
-    },
-
-    // If you hit `enter`, we're through editing the item.
-    updateOnEnter: function(e) {
-      if (e.keyCode == 13) this.close();
-    },
-
-    // Remove the item, destroy the model.
-    clear: function() {
-      this.model.destroy();
+      }).error(function() {
+        defer.resolve([]);
+      
+      });
+      return defer.promise;
     }
-
-  });
-
-  // The Application
-  // ---------------
-
-  var AppView = Backbone.View.extend({
-
-    el: $("#skratchapp"),
-
-    events: {
-      "keypress #new-skratch":  "createOnEnter"
-    },
-
-    initialize: function() {
-
-      this.input = this.$("#new-skratch");
-
-      this.listenTo(Skratches, 'add', this.addOne);
-      this.listenTo(Skratches, 'reset', this.addAll);
-      this.listenTo(Skratches, 'all', this.render);
-
-      this.footer = this.$('footer');
-      this.main = $('#main');
-
-      Skratches.fetch();
-    },
-
-    render: function() {
-      if (Skratches.length) {
-        this.main.show();
-        this.footer.show();
-      } else {
-        this.main.hide();
-        this.footer.hide();
-      }
-    },
-
-    addOne: function(skratch) {
-      var view = new SkratchView({model: skratch});
-      this.$("#skratch-list").prepend(view.render().el);
-    },
-
-    addAll: function() {
-      Skratches.each(this.addOne, this);
-    },
-
-    createOnEnter: function(e) {
-      if (e.keyCode != 13) return;
-      if (!this.input.val()) return;
-
-      Skratches.create({text: this.input.val()});
-      this.input.val('');
-    }
-
-  });
-
-  var App = new AppView;
+  }
 
 });
+
