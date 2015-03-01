b718814022b21450b742780b55fdaec9b8a10287
Eliot Baker <eliotbaker@gmail.com>
2015-03-01 13:05:26 -0500

    create scratch. update scope

--- public/skratchpad.js
+++ public/skratchpad.js
@@ -1,5 +1,4 @@
-//
-var app = angular.module('skratchpadApp',[]);
+var app = angular.module('skratchpadApp', []);
 
 app.directive('skratchpad', function(skratchesFactory) {
   return {
@@ -8,12 +7,34 @@ app.directive('skratchpad', function(skratchesFactory) {
     replace: true,
     link: function(scope, elem, attrs){
       skratchesFactory.getAll().then(function(data){
-        console.log('data', data);
         scope.skratches = data;
       });
     },
+    controller: function($scope) {
+      $scope.addSkratch = function(){
+        skratchesFactory.post({text: $scope.text}).then(function(data){
+          $scope.skratches.unshift(data);
+        });
+      }
+    },
     templateUrl: 'skratchpad.html'
   }
+});
+
+app.directive('ngEnter', function () {
+  return {
+    restict: 'A',
+    require: '^skratchpad',
+    link: function (scope, element, attrs, SkratchpadCtrl) {
+    element.bind("keydown keypress", function (event) {
+      if(event.which === 13) {
+        scope.$eval(attrs.ngEnter);
+        element.val('');
+        event.preventDefault();
+        }
+    });
+    }
+  };
 });
 
 app.factory('skratchesFactory', function($http, $q) {
@@ -21,14 +42,21 @@ app.factory('skratchesFactory', function($http, $q) {
     getAll: function() {
       var defer = $q.defer()
       $http.get('/api/skratches').success(function(data){
-        // return data;
         defer.resolve(data);
-
       }).error(function() {
         defer.resolve([]);
-      
       });
       return defer.promise;
+    },
+    post: function(data) {
+      var defer = $q.defer()
+      $http.post('/api/skratches', data).success(function(data){
+        defer.resolve(data);
+      }).error(function() {
+        defer.resolve(null);
+      });
+      return defer.promise;
+   
     }
   }
 
